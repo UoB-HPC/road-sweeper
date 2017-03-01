d88771054e1d97345fa0331f9d5f5d7eee4af302
Tom Deakin <tom.deakin@bristol.ac.uk>
2017-03-01 18:14:04 +0000

    Send multiple messages per octant sweep

--- serialsweep.c
+++ serialsweep.c
@@ -19,44 +19,48 @@ void serial_sweep(mpistate mpi, options opt) {
     for (int j = 0; j < 2; j++) {
       for (int i = 0; i < 2; i++) {
 
-        /* Receive payload from upwind neighbours */
-        if (j == 0) {
-          MPI_Recv(ybuf, ycount, MPI_DOUBLE, mpi.yhi, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
-        else {
-          MPI_Recv(ybuf, ycount, MPI_DOUBLE, mpi.ylo, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
+        /* Loop over messages to send per octant */
+        for (int m = 0; m < opt.msgs; m++) {
 
-        if (k == 0) {
-          MPI_Recv(zbuf, zcount, MPI_DOUBLE, mpi.zhi, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
-        else {
-          MPI_Recv(zbuf, zcount, MPI_DOUBLE, mpi.zlo, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
+          /* Receive payload from upwind neighbours */
+          if (j == 0) {
+            MPI_Recv(ybuf, ycount, MPI_DOUBLE, mpi.yhi, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+          }
+          else {
+            MPI_Recv(ybuf, ycount, MPI_DOUBLE, mpi.ylo, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+          }
 
-        /* Do work */
-        printf("Rank %d starting\n", mpi.rank);
-        compute();
+          if (k == 0) {
+            MPI_Recv(zbuf, zcount, MPI_DOUBLE, mpi.zhi, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+          }
+          else {
+            MPI_Recv(zbuf, zcount, MPI_DOUBLE, mpi.zlo, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+          }
 
-        /* Send payload to downwind neighbours */
-        MPI_Waitall(2, req, MPI_STATUS_IGNORE);
+          /* Do work */
+          printf("Rank %d starting\n", mpi.rank);
+          compute();
 
-        if (j == 0) {
-          MPI_Isend(ybuf, ycount, MPI_DOUBLE, mpi.ylo, 0, MPI_COMM_WORLD, req+0);
-        }
-        else {
-          MPI_Isend(ybuf, ycount, MPI_DOUBLE, mpi.yhi, 0, MPI_COMM_WORLD, req+0);
-        }
+          /* Send payload to downwind neighbours */
+          MPI_Waitall(2, req, MPI_STATUS_IGNORE);
 
-        if (k == 0) {
-          MPI_Isend(zbuf, zcount, MPI_DOUBLE, mpi.zlo, 0, MPI_COMM_WORLD, req+1);
-        }
-        else {
-          MPI_Isend(zbuf, zcount, MPI_DOUBLE, mpi.zhi, 0, MPI_COMM_WORLD, req+1);
-        }
+          if (j == 0) {
+            MPI_Isend(ybuf, ycount, MPI_DOUBLE, mpi.ylo, 0, MPI_COMM_WORLD, req+0);
+          }
+          else {
+            MPI_Isend(ybuf, ycount, MPI_DOUBLE, mpi.yhi, 0, MPI_COMM_WORLD, req+0);
+          }
 
-      }
-    }
-  }
+          if (k == 0) {
+            MPI_Isend(zbuf, zcount, MPI_DOUBLE, mpi.zlo, 0, MPI_COMM_WORLD, req+1);
+          }
+          else {
+            MPI_Isend(zbuf, zcount, MPI_DOUBLE, mpi.zhi, 0, MPI_COMM_WORLD, req+1);
+          }
+
+        } /* End msgs loop */
+      } /* End i loop */
+    } /* End j loop */
+  } /* End k loop */
 }
 
