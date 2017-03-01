57a29d857401e8f45f5ddf880d995997b91c31a6
Tom Deakin <tom.deakin@bristol.ac.uk>
2017-03-01 12:46:14 +0000

    Add struct fields for 2D decomposition

--- comms.h
+++ comms.h
@@ -5,6 +5,8 @@
 
 #pragma once
 
+#include "options.h"
+
 typedef struct mpistate {
 
   /* Level of thread support provided by MPI implementation */
@@ -16,5 +18,21 @@ typedef struct mpistate {
   /* Number of ranks in MPI_COMM_WORLD */
   int nprocs;
 
+  /* Number of ranks in 2D decomposition */
+  int npey;
+  int npez;
+
+  /* Process rank in 2D decomposition */
+  int y;
+  int z;
+
+  /* Neighbour ranks - 2D decomposition of 3D domain */
+  int yhi;
+  int ylo;
+  int zhi;
+  int zlo;
+
 } mpistate;
+
+void decompose(mpistate *mpi, options opt);
 
