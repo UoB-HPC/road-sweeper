6eb0722933e65ccaebae858fc51375c350a3baaa
Tom Deakin <tom.deakin@bristol.ac.uk>
2017-03-01 14:08:10 +0000

    Add octant loop to serial sweep

--- serialsweep.c
+++ serialsweep.c
@@ -13,16 +13,49 @@ void serial_sweep(mpistate mpi, options opt) {
   /* Send requests */
   MPI_Request req[2] = {MPI_REQUEST_NULL, MPI_REQUEST_NULL};
 
-  /* Receive payload from upwind neighbours */
-  MPI_Recv(ybuf, ycount, MPI_DOUBLE, mpi.ylo, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  MPI_Recv(zbuf, zcount, MPI_DOUBLE, mpi.zlo, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  /* Octant loops - 0 is stepping backwards, 1 is stepping forwards */
+  for (int k = 0; k < 2; k++) {
+    for (int j = 0; j < 2; j++) {
+      for (int i = 0; i < 2; i++) {
 
-  printf("Rank %d starting\n", mpi.rank);
-  sleep(5);
+        /* Receive payload from upwind neighbours */
+        if (j == 0) {
+          MPI_Recv(ybuf, ycount, MPI_DOUBLE, mpi.yhi, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+        else {
+          MPI_Recv(ybuf, ycount, MPI_DOUBLE, mpi.ylo, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
 
-  /* Send payload to downwind neighbours */
-  MPI_Waitall(2, req, MPI_STATUS_IGNORE);
-  MPI_Isend(ybuf, ycount, MPI_DOUBLE, mpi.yhi, 0, MPI_COMM_WORLD, req+0);
-  MPI_Isend(zbuf, zcount, MPI_DOUBLE, mpi.zhi, 0, MPI_COMM_WORLD, req+1);
+        if (k == 0) {
+          MPI_Recv(zbuf, zcount, MPI_DOUBLE, mpi.zhi, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+        else {
+          MPI_Recv(zbuf, zcount, MPI_DOUBLE, mpi.zlo, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+
+        /* Do work */
+        printf("Rank %d starting\n", mpi.rank);
+        sleep(5);
+
+        /* Send payload to downwind neighbours */
+        MPI_Waitall(2, req, MPI_STATUS_IGNORE);
+
+        if (j == 0) {
+          MPI_Isend(ybuf, ycount, MPI_DOUBLE, mpi.ylo, 0, MPI_COMM_WORLD, req+0);
+        }
+        else {
+          MPI_Isend(ybuf, ycount, MPI_DOUBLE, mpi.yhi, 0, MPI_COMM_WORLD, req+0);
+        }
+
+        if (k == 0) {
+          MPI_Isend(zbuf, zcount, MPI_DOUBLE, mpi.zlo, 0, MPI_COMM_WORLD, req+1);
+        }
+        else {
+          MPI_Isend(zbuf, zcount, MPI_DOUBLE, mpi.zhi, 0, MPI_COMM_WORLD, req+1);
+        }
+
+      }
+    }
+  }
 }
 
