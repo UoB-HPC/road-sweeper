49f02e149dae97723fcf84385f7676e51564455e
Tom Deakin <tom.deakin@bristol.ac.uk>
2017-03-02 13:02:09 +0000

    Send msglen doubles

--- serialsweep.c
+++ serialsweep.c
@@ -6,9 +6,6 @@
 /* Perform a vanilla KBA sweep without using OpenMP threads */
 void serial_sweep(mpistate mpi, options opt, messages msg) {
 
-  int ycount = 0;
-  int zcount = 0;
-
   /* Send requests */
   MPI_Request req[2] = {MPI_REQUEST_NULL, MPI_REQUEST_NULL};
 
@@ -22,17 +19,17 @@ void serial_sweep(mpistate mpi, options opt, messages msg) {
 
           /* Receive payload from upwind neighbours */
           if (j == 0) {
-            MPI_Recv(msg.ybuf, ycount, MPI_DOUBLE, mpi.yhi, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(msg.ybuf, opt.msglen, MPI_DOUBLE, mpi.yhi, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
           }
           else {
-            MPI_Recv(msg.ybuf, ycount, MPI_DOUBLE, mpi.ylo, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(msg.ybuf, opt.msglen, MPI_DOUBLE, mpi.ylo, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
           }
 
           if (k == 0) {
-            MPI_Recv(msg.zbuf, zcount, MPI_DOUBLE, mpi.zhi, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(msg.zbuf, opt.msglen, MPI_DOUBLE, mpi.zhi, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
           }
           else {
-            MPI_Recv(msg.zbuf, zcount, MPI_DOUBLE, mpi.zlo, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(msg.zbuf, opt.msglen, MPI_DOUBLE, mpi.zlo, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
           }
 
           /* Do "work" */
@@ -42,17 +39,17 @@ void serial_sweep(mpistate mpi, options opt, messages msg) {
           MPI_Waitall(2, req, MPI_STATUS_IGNORE);
 
           if (j == 0) {
-            MPI_Isend(msg.ybuf, ycount, MPI_DOUBLE, mpi.ylo, 0, MPI_COMM_WORLD, req+0);
+            MPI_Isend(msg.ybuf, opt.msglen, MPI_DOUBLE, mpi.ylo, 0, MPI_COMM_WORLD, req+0);
           }
           else {
-            MPI_Isend(msg.ybuf, ycount, MPI_DOUBLE, mpi.yhi, 0, MPI_COMM_WORLD, req+0);
+            MPI_Isend(msg.ybuf, opt.msglen, MPI_DOUBLE, mpi.yhi, 0, MPI_COMM_WORLD, req+0);
           }
 
           if (k == 0) {
-            MPI_Isend(msg.zbuf, zcount, MPI_DOUBLE, mpi.zlo, 0, MPI_COMM_WORLD, req+1);
+            MPI_Isend(msg.zbuf, opt.msglen, MPI_DOUBLE, mpi.zlo, 0, MPI_COMM_WORLD, req+1);
           }
           else {
-            MPI_Isend(msg.zbuf, zcount, MPI_DOUBLE, mpi.zhi, 0, MPI_COMM_WORLD, req+1);
+            MPI_Isend(msg.zbuf, opt.msglen, MPI_DOUBLE, mpi.zhi, 0, MPI_COMM_WORLD, req+1);
           }
 
         } /* End nchunks loop */
