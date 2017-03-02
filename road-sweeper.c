7a741030fac5364988912175c890a3f7ffa0c47a
Tom Deakin <tom.deakin@bristol.ac.uk>
2017-03-02 21:53:42 +0000

    Print percentage of time in comms

--- road-sweeper.c
+++ road-sweeper.c
@@ -86,7 +86,7 @@ int main(int argc, char *argv[]) {
     printf("  Total:    %11.6lf s\n", tock-tick);
     printf("  Sweeping: %11.6lf s\n", time.sweeping);
     printf("  Setup:    %11.6lf s\n", time.setup);
-    printf("  Comms:    %11.6lf s\n", time.comms);
+    printf("  Comms:    %11.6lf s (%.1lf%%)\n", time.comms, time.comms/time.sweeping*100.0);
   }
 
   MPI_Finalize();
