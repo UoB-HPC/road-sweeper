193b9a8480af4c9940cdeee30c33f2257bd18c7e
Tom Deakin <tom.deakin@bristol.ac.uk>
2017-03-02 13:54:33 +0000

    Comment the new sweep header

--- sweep.h
+++ sweep.h
@@ -1,8 +1,7 @@
 
 /*
- * Vanilla serial KBA sweeper
- * For each octant, groups are computed serially in turn.
- * We send a message of all the angles on the face for one energy group.
+ * Sweep options
+ * Header provides method declarations for various sweeper options.
  */
 
 #pragma once
@@ -10,5 +9,10 @@
 #include "comms.h"
 #include "options.h"
 
+/*
+ * Vanilla serial KBA sweeper
+ * For each octant, groups are computed serially in turn.
+ * We send a message of all the angles on the face for one energy group.
+ */
 void serial_sweep(mpistate mpi, options opt, messages msg);
 
